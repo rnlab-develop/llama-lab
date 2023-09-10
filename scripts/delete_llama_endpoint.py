771352358b1efbc25c1808977463a09dfe464716
Neal Harrington <ngharrington@users.noreply.github.com>
2023-09-09 23:11:53 -0400

    Fix script

--- scripts/delete_llama_endpoint.py
+++ scripts/delete_llama_endpoint.py
@@ -2,12 +2,9 @@ from google.cloud import aiplatform
 
 if __name__ == "__main__":
 
-    endpoint = aiplatform.Endpoint(
-        project="sandbox-378304",
-        location="us-central1",
-        endpoint_name="llama2-70b-chat-001_endpoint"
-    )
+    endpoints = aiplatform.Endpoint.list(filter="display_name=llama2-7b-chat-001_endpoint")
+    endpoint = endpoints[0]
 
     print("Deleting Llama Endpoint...")
-    endpoint.delete()
+    endpoint.delete(force=True)
     print("Llama Endpoint deleted.")
